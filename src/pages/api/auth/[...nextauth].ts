fc6534d7110bd0f9c7f03f7ec0fb13d4aa7d8eba
alvarosena <alvarosenacs.c@gmail.com>
2022-07-04 12:38:59 -0300

    Connect with FaunaDB nosql and save user data.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+// FQL
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
+import { fauna } from "../../../lib/fuana";
 
 export default NextAuth({
   providers: [
@@ -12,5 +16,39 @@ export default NextAuth({
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({user}) {
+      const { email } = user
+
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 })
